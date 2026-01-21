b257436cf421aa1d34b3586311bcd1d2d977f560
ella <lydbsdud@gmail.com>
2026-01-21 16:38:41 +0900

    refactor: 불필요한 import 삭제

--- src/main/java/com/example/doktoribackend/user/controller/UserController.java
+++ src/main/java/com/example/doktoribackend/user/controller/UserController.java
@@ -15,7 +15,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
