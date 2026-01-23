bbd4ceb77e7787a7de653c6fef2c042737aac0e5
ella <lydbsdud@gmail.com>
2026-01-23 11:12:23 +0900

    feat: 의존성 추가

--- src/main/java/com/example/doktoribackend/common/constants/ValidationConstant.java
+++ src/main/java/com/example/doktoribackend/common/constants/ValidationConstant.java
@@ -7,5 +7,7 @@ public final class ValidationConstant {
     public static final int NICKNAME_MAX_LENGTH = 20;
     public static final int INTRO_MAX_LENGTH = 300;
     public static final int PROFILE_IMAGE_PATH_MAX_LENGTH = 512;
+
+    public static final long MAX_FILE_SIZE = 5_242_880L;
 }
 
