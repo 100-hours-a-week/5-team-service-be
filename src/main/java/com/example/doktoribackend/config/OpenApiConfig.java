196f8eb5164ef8a43e386735ec8839b8dbaadd64
ella <lydbsdud@gmail.com>
2026-01-23 13:31:47 +0900

    doc: swagger server 명시

--- src/main/java/com/example/doktoribackend/config/OpenApiConfig.java
+++ src/main/java/com/example/doktoribackend/config/OpenApiConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,6 +14,10 @@ class OpenApiConfig {
     @Bean
     OpenAPI openApi() {
         String bearerSchemeName = "bearerAuth";
+
+        Server devServer = new Server();
+        devServer.setUrl("https://dev.doktori.kr");
+        devServer.setDescription("Development Server");
 
         return new OpenAPI()
                 .info(new Info()
