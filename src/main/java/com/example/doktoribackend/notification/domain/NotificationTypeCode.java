aae1b85d8fdc96f770221a7db8e0d8fecf6518c6
ella <lydbsdud@gmail.com>
2026-01-25 18:58:57 +0900

    feat: 알림, 알림 타입 domain - Notification - NotificationType

---
+++ src/main/java/com/example/doktoribackend/notification/domain/NotificationTypeCode.java
@@ -0,0 +1,8 @@
+package com.example.doktoribackend.notification.domain;
+
+public enum NotificationTypeCode {
+    ROUND_START_10M_BEFORE,
+    BOOK_REVIEW_CHECKED,
+    BOOK_REVIEW_DEADLINE_24H_BEFORE,
+    BOOK_REVIEW_DEADLINE_30M_BEFORE
+}
